0812bf545633f931abae4947bc4837ad809e87fb
trevorthomas98 <139725457+trevorthomas98@users.noreply.github.com>
2023-08-06 20:58:51 -0700

    Update splitplatforms.py

--- my-thesis/splitplatforms.py
+++ my-thesis/splitplatforms.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Replace 'your_file.csv' with the actual path to your CSV file
-csv_file_path = 'trevor.thomas/df_monitored_tor.csv'
+csv_file_path = '/home/trevor.thomas/df_monitored_tor.csv'
 
 # Read the CSV file into a pandas DataFrame
 df = pd.read_csv(csv_file_path)
