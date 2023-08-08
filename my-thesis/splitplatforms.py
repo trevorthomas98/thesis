7d7770054aa1e75ef7d29bc534354987fe5b41f0
trevorthomas98 <139725457+trevorthomas98@users.noreply.github.com>
2023-08-07 20:07:39 -0700

    Update splitplatforms.py

--- my-thesis/splitplatforms.py
+++ my-thesis/splitplatforms.py
@@ -1,29 +1,28 @@
 import pandas as pd
 
 youtubecsv_file_path = '/data/timothy.walsh/hayden_monitored_tor_youtube.csv'
-facebookcsv_file_path = '/data/timothy.walsh/hayden_monitored_tor_facebook.csv'
-vimeocsv_file_path = '/data/timothy.walsh/hayden_monitored_tor_vimeo.csv'
-rumblecsv_file_path = '/data/timothy.walsh/hayden_monitored_tor_rumble.csv'
+#facebookcsv_file_path = '/data/timothy.walsh/hayden_monitored_tor_facebook.csv'
+#vimeocsv_file_path = '/data/timothy.walsh/hayden_monitored_tor_vimeo.csv'
+#rumblecsv_file_path = '/data/timothy.walsh/hayden_monitored_tor_rumble.csv'
 
 # Read the CSV file into a pandas DataFrame
 youtubedf = pd.read_csv(youtubecsv_file_path)
 print("Youtube df.head is")
 print(youtubedf.head)
 
-facebookdf = pd.read_csv(facebookcsv_file_path)
-print("Facebook df.head is")
-print(Facebookdf.head)
+#facebookdf = pd.read_csv(facebookcsv_file_path)
+#print("Facebook df.head is")
+#print(Facebookdf.head)
 
-vimeodf = pd.read_csv(vimeocsv_file_path)
-print("Vimeo df.head is")
-print(Vimeodf.head)
+#vimeodf = pd.read_csv(vimeocsv_file_path)
+#print("Vimeo df.head is")
+#print(Vimeodf.head)
 
-rumbledf = pd.read_csv(rumblecsv_file_path)
-print("Rumble df.head is")
-print(Rumbledf.head)
+#rumbledf = pd.read_csv(rumblecsv_file_path)
+#print("Rumble df.head is")
+#print(Rumbledf.head)
 
-youtubedf.to_csv('youtubedata.csv', index=False)
-facebookdf.to_csv('facebookdata.csv', index=False)
-vimeodf.to_csv('vimeodata.csv', index=False)
-rumbledf.to_csv('rumbledata.csv', index=False)
+with open('X_train_NoDef.pkl', 'wb') as handle:
+    pickle.dump(youtubedf)
+
 
