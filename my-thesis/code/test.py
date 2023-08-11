ba7d660c918e73e9537bf6e7c670ea1804fd7adb
trevorthomas98 <139725457+trevorthomas98@users.noreply.github.com>
2023-08-11 14:04:42 -0700

    Update test.py

--- my-thesis/code/test.py
+++ my-thesis/code/test.py
@@ -2,9 +2,12 @@ import pandas as pd
 import pickle
 import learning_5000
 
+pickle_file_path = '/home/trevor.thomas/thesis/my-thesis/youtubedf.pkl'
+df = pd.read_pickle(pickle_file_path)
+
 def main():
 
 
-    learning_5000.learning(youtubedf)
+    learning_5000.learning(df)
 
 main()
