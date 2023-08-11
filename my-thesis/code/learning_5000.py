49f83e2db9e55b7526e47a303bdd9bba1f4b6504
trevorthomas98 <139725457+trevorthomas98@users.noreply.github.com>
2023-08-11 14:41:20 -0700

    Update learning_5000.py

--- my-thesis/code/learning_5000.py
+++ my-thesis/code/learning_5000.py
@@ -20,7 +20,7 @@ def learning(df):
     df = df.drop(['entry_ip'], axis=1)
     df = df.drop(['crawl_identifier'], axis=1)
     df = df.drop(['region'], axis=1)
-    df = df.drop(['genre'], axis=1)
+    #df = df.drop(['genre'], axis=1)
     X = df
 
 
