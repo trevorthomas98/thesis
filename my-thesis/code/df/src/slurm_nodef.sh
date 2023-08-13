eb0bbba07e006de5c8bed0c1d7452078331a1a23
trevorthomas98 <139725457+trevorthomas98@users.noreply.github.com>
2023-08-13 10:35:16 -0700

    Update slurm_nodef.sh

--- my-thesis/code/df/src/slurm_nodef.sh
+++ my-thesis/code/df/src/slurm_nodef.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --time=10-00:00:00
                                                            
-#SBATCH -o /home/trevor.thomas/thesis/new/my-thesis/runs/closedworld_nodef_run.%j.out
+#SBATCH -o /home/trevor.thomas/thesis/my-thesis/runs/closedworld_nodef_run.%j.out
 #SBATCH --partition=barton
 #SBATCH --mem=128000
 #SBATCH --gres=gpu:1
