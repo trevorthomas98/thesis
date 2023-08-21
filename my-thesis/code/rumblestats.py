75529f4bd334b6da855fa694cb5bc19eebf17af5
trevorthomas98 <139725457+trevorthomas98@users.noreply.github.com>
2023-08-20 20:22:16 -0700

    Update rumblestats.py

--- my-thesis/code/rumblestats.py
+++ my-thesis/code/rumblestats.py
@@ -7,8 +7,6 @@ import learning_NoDef
 pickle_file_path = '/home/trevor.thomas/rumbledf.pkl'
 #pickle_file_path = '/home/trevor.thomas/facebookdf.pkl'
 df = pd.read_pickle(pickle_file_path)
-print(df)
-
 sorted_df = df.sort_values(by=['num_of_packets'], ascending=True)
 
 print(sorted_df['num_of_packets'])
