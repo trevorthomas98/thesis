18ba10812848d32882f19709fb4577a563d85ad9
trevorthomas98 <139725457+trevorthomas98@users.noreply.github.com>
2023-08-20 20:15:02 -0700

    Update rumblestats.py

--- my-thesis/code/rumblestats.py
+++ my-thesis/code/rumblestats.py
@@ -10,4 +10,7 @@ df = pd.read_pickle(pickle_file_path)
 print(df)
 print(df['num_of_packets'])
 print(df['num_of_packets'].unique())
-print(df.drop_duplicates(subset = 'num_of_packets'))
+sorted_df = df.sort_values(by=['num_of_packets'], ascending=True)
+print(sorted_df)
+print(sorted_df['num_of_packets'])
+
