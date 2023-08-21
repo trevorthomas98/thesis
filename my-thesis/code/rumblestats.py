ef1f706c93d82dc29b140a35f219470fb872b86f
trevorthomas98 <139725457+trevorthomas98@users.noreply.github.com>
2023-08-20 20:48:54 -0700

    Update rumblestats.py

--- my-thesis/code/rumblestats.py
+++ my-thesis/code/rumblestats.py
@@ -9,6 +9,56 @@ pickle_file_path = '/home/trevor.thomas/rumbledf.pkl'
 df = pd.read_pickle(pickle_file_path)
 sorted_df = df.sort_values(by=['num_of_packets'], ascending=True)
 
+print('rumble stats')
 print(sorted_df['num_of_packets'])
 print(sorted_df.head(50))
+
+import pandas as pd
+import pickle
+import learning_NoDef
+
+pickle_file_path = '/home/trevor.thomas/youtubedf.pkl'
+#pickle_file_path = '/home/trevor.thomas/vimeodf.pkl'
+#pickle_file_path = '/home/trevor.thomas/rumbledf.pkl'
+#pickle_file_path = '/home/trevor.thomas/facebookdf.pkl'
+df = pd.read_pickle(pickle_file_path)
+sorted_df = df.sort_values(by=['num_of_packets'], ascending=True)
+
+print('youtube stats')
+print(sorted_df['num_of_packets'])
+print(sorted_df.head(50))
+
+import pandas as pd
+import pickle
+import learning_NoDef
+
+#pickle_file_path = '/home/trevor.thomas/youtubedf.pkl'
+pickle_file_path = '/home/trevor.thomas/vimeodf.pkl'
+#pickle_file_path = '/home/trevor.thomas/rumbledf.pkl'
+#pickle_file_path = '/home/trevor.thomas/facebookdf.pkl'
+df = pd.read_pickle(pickle_file_path)
+sorted_df = df.sort_values(by=['num_of_packets'], ascending=True)
+
+print('vimeo stats')
+print(sorted_df['num_of_packets'])
+print(sorted_df.head(50))
+
+
+import pandas as pd
+import pickle
+import learning_NoDef
+
+#pickle_file_path = '/home/trevor.thomas/youtubedf.pkl'
+#pickle_file_path = '/home/trevor.thomas/vimeodf.pkl'
+#pickle_file_path = '/home/trevor.thomas/rumbledf.pkl'
+pickle_file_path = '/home/trevor.thomas/facebookdf.pkl'
+df = pd.read_pickle(pickle_file_path)
+sorted_df = df.sort_values(by=['num_of_packets'], ascending=True)
+
+print('facebook stats')
+print(sorted_df['num_of_packets'])
+print(sorted_df.head(50))
+
+
+
 
