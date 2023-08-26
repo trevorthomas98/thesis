4b901722a2e839abcc3e04dcf6a6fa74e8edac8a
trevorthomas98 <139725457+trevorthomas98@users.noreply.github.com>
2023-08-26 16:24:55 -0700

    Update runafterdfbuilt.py

--- my-thesis/code/runafterdfbuilt.py
+++ my-thesis/code/runafterdfbuilt.py
@@ -4,8 +4,8 @@ import learning_NoDef
 
 #pickle_file_path = '/home/trevor.thomas/youtubedf.pkl'
 #pickle_file_path = '/home/trevor.thomas/vimeodf.pkl'
-pickle_file_path = '/home/trevor.thomas/rumbledf.pkl'
-#pickle_file_path = '/home/trevor.thomas/facebookdf.pkl'
+#pickle_file_path = '/home/trevor.thomas/rumbledf.pkl'
+pickle_file_path = '/home/trevor.thomas/facebookdf.pkl'
 df = pd.read_pickle(pickle_file_path)
 print(df)
 def main():
